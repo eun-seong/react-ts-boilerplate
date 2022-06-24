1e552e36a38b470d6c4c7aa901baa91741797f35
eunseonglee <eunseongofficial@gmail.com>
2022-06-24 22:22:41 +0900

    chore: 패키지 버전 업데이트

--- client/webpack.config.dev.js
+++ client/webpack.config.dev.js
@@ -3,20 +3,17 @@ const common = require('./webpack.config.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'source-map',
-  devServer: {
-    contentBase: './public',
-    host: 'localhost',
-    port: 9000,
-    historyApiFallback: true,
-    hot: true,
-    overlay: true,
-    stats: 'errors-only',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        host: 'localhost',
+        port: 9000,
+        historyApiFallback: true,
+        hot: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
 });
